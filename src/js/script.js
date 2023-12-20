51665fb6c2f935f36e9cfbfd4cb2aeebbbea5010
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2023-12-20 13:35:23 +0200

    HW 3 complete

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+1.
+let name=prompt('What your name?');
+let age=prompt('How old are you?');
+
+2.
+//+
+
+const a=prompt('Enter first number');
+const b=prompt('Enter second number');
+const c=(+a + +b);
+alert(c);
+console.log(c);
+
+//-
+
+const d=prompt('Enter first number');
+const e=prompt('Enter second number');
+const f=(d - e );
+alert(f);
+console.log(f);
+
+//*
+
+const j=prompt('Enter first number');
+const k=prompt('Enter second number');
+const l=(j * k);
+alert(l);
+console.log(l);
+
+// /
+
+const m=prompt('Enter first number');
+const n=prompt('Enter second number');
+const o=(m / n);
+alert(o);
+console.log(o);
