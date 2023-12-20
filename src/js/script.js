b8242973e18a93730c08f21bd539015ee7b633f5
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2023-12-20 13:24:11 +0200

    HW 2 complete

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+//task1
+
+let admin;
+let name;
+name='John';
+admin=name;
+alert(admin);
+
+//task2
+
+let ball;
+ball='Eternity';
+const userName='Identifying';
+
+//task3
+
+const sum='10'+10;
+console.log(sum);
