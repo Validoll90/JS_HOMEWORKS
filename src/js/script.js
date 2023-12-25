6b51e76c6a43f5384cf3502a343b6fbe42b5cdaa
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2023-12-25 14:40:50 +0200

    HW 4 complete

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+1.
+
+let firstString=prompt('Your name');
+let secondString=prompt('Your age');
+let thirdString=prompt('Your gender');
+const result=(firstString + secondString + thirdString);
+alert(result);
+console.log(result);
+
+2.
+
+let firstChar=1;
+let secondChar=2;
+let thirdChar=3;
+let fourthChar=4;
+let fifthChar=5;
+console.log(firstChar, secondChar, thirdChar, fourthChar, fifthChar );
