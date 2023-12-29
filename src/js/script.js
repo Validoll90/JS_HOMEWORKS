f8a880eb5e854221452cb997f047555fb1f77429
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2023-12-29 19:56:08 +0200

    HW16

--- src/js/script.js
+++ src/js/script.js
@@ -3,35 +3,30 @@
 const age = +prompt('Введіть ваш вік');
 const town= prompt('З якого ви міста');
 const sport=prompt('Ваш улюблений вид спорту');
+const football='David Backham';
+const tennis='Serena Williams';
+const box='Antony Joshua';
 
 
-if (age==false) {
+if (age==false ) {
     alert('Шкода, що Ви не захотіли ввести свій вік');
 }
 
-if (town === 'Київ') {
+if (town === 'Київ'.toLowerCase()) {
     alert('Ви живете в столиці України!');
-} else if (town === 'Лондон')  {
+} else if (town === 'Лондон'.toLowerCase())  {
     alert('Ви живете в столиці Англії!');
-} else if (town === 'Вашингтон') {
+} else if (town === 'Вашингтон'.toLowerCase()) {
     alert('Ви живете в столиці Америки!');
 }  else {
     alert(`Ти живеш у місті ${town}`);
 }
-
-const football='David Backham';
-const tennis='Serena Williams';
-const box='Antony Joshua';
-switch (sport) {
-    case 'Футбол':
-        alert(`Круто! Хочеш стати ${football}`);
-        break;
-    case 'Теніс':
-        alert(`Круто! Хочеш стати ${tennis}`);
-        break;
-    case 'Бокс':
-        alert(`Круто! Хочеш стати ${box}`);
-        break;
-    default:
-        alert('Шкода, що Ви не захотіли ввести свій вид спорту');
+if (sport === 'Футбол'.toLowerCase()) {
+    alert(`Круто! Хочеш стати ${football}`);
+} else if (sport === 'Теніс'.toLowerCase()) {
+    alert(`Круто! Хочеш стати ${tennis}`);
+} else if (sport === 'Бокс'.toLowerCase()) {
+    alert(`Круто! Хочеш стати ${box}`);
+} else {
+    alert('Шкода, що Ви не захотіли ввести свій вид спорту');
 }
