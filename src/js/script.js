d990d46544192ccc6c3253d1fc76cd5cc9320759
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-01-03 10:09:40 +0200

    HW12

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+//sum+
+
+const num1=Number(prompt('Enter first number'));
+const num2=Number(prompt('Enter second number'));
+const result1= num1 + num2;
+alert(result1);
+console.log(typeof result1);
+
+//-
+const num3=prompt('Enter first number');
+const num4=prompt('Enter second number');
+const result2= num3 - num4;
+alert(result2);
+console.log(typeof result2);
+
+//multi*
+
+const num5=prompt('Enter first number');
+const num6=prompt('Enter second number');
+const result3= num5 * num6;
+alert(result3);
+console.log(typeof result3);
+
+//divid/
+
+const num7=prompt('Enter first number');
+const num8=prompt('Enter second number');
+const result4= num7 / num8;
+alert(result4);
+console.log(typeof result4);
