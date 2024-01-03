bdb51d116d58e7404e5be78e27b90e6127006ddd
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-01-03 11:06:58 +0200

    HW15

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const numb1=Number (prompt('Write first number'));
+const numb2=Number (prompt('Write second number'));
+const numb3=Number (prompt('Write third number'));
+const result = (numb1 + numb2 + numb3) / 3;
+alert(result);
