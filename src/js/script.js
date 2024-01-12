b08e709ac8fbda5f162319e578a60928ab75aec4
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-01-12 16:24:16 +0200

    HW24

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const num = +prompt('Введіть число');
+let minDiv=0;
+for (let i =2; i < num; i++) {
+    if (num % i === 0) {
+        minDiv = i;
+    } else {
+        num < 1;
+        console.log(NaN);
+    }
+}
+console.log(`${minDiv}`);
