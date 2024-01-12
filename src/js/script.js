888c05ec83a94cde21561dce3f305f105654a123
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-01-12 15:53:09 +0200

    HW23

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+let square = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    square += arr[i]**2;
+} console.log(sum, square);
