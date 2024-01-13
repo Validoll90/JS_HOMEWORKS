a17e5479c6404cbb1c61798117ec774a58eef926
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-01-13 17:36:27 +0200

    HW27

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+let numb = prompt('Введіть число');
+let degree = prompt('Введіть ступінь');
+function numbDeg (numb, degree = 1) {
+ if (isNaN(numb) && isNaN(degree)) return "помилка";
+return (+numb) ** (+degree);
+}
+const result = numbDeg(numb, degree);
+alert(result);
