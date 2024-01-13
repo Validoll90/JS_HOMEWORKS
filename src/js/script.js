dafe1393003a2da41b3fc0f67b4bbb9c10a38515
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-01-13 10:36:07 +0200

    HW25

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let sum =0;
+outerLoop:
+for (let i = 1; i <=20; i++) {
+    if (i % 2 !== 0) {
+        sum += i;
+    } else {
+        i % 2 === 0;
+        alert(`${sum}`);
+        break outerLoop;
+    }
+} console.log(`${sum}`);
