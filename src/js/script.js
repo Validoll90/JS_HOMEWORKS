796c06eaba867cdce8536906c89bfd8677db6f40
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-01-13 17:00:52 +0200

    HW26

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function generateKey(length, characters) {
+    let str = '';
+    while (str.length < length) {
+        str += characters[Math.floor(Math.random() * characters.length)];
+    }
+    return str;
+}
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+console.log(key);
