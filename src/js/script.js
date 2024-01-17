62eef1dd06b0ca485693d581bec514e479807bd8
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-01-17 09:36:08 +0200

    HW29

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function newArr(array){
+
+    const positiveArr = [];
+    if(array.length === 0) return "Array is empty";
+
+    for(let i = 0; i < array.length; i++){
+        if(array[i] >= 0) positiveArr.push(array[i]);
+    }
+
+    return positiveArr.length !== 0 ? positiveArr : null;
+}
+const arr = [1, 2, 3, -1, -2, -3];
+
+console.log(newArr(arr));
