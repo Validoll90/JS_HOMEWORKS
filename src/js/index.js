3b7d58bdf0a5879ead06628f17cc9cad43f36009
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-01-31 13:33:41 +0200

    HW37

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,32 @@
-function sum(a, b) {
-  return a + b;
+function parseUrl(url) {
+  const parsedUrl = new URL(url);
+  return {
+    href: parsedUrl.href,
+    protocol: parsedUrl.protocol,
+    host: parsedUrl.host,
+    hostname: parsedUrl.hostname,
+    pathname: parsedUrl.pathname,
+    search: parsedUrl.search,
+    hash: parsedUrl.hash,
+    origin: parsedUrl.origin,
+  };
 }
-sum(4, 5);
 
-function min(a, b) {
-  return a - b;
-}
-min(3, 1);
-function mult(a, b) {
-  return a * b;
-}
-mult(8, 5);
+const result = parseUrl("https://example.com:8080/path/name?query=123#hash");
+console.log(result);
+
+const yourUrl = function () {
+  return {
+    href: window.location.href,
+    protocol: window.location.protocol,
+    host: window.location.host,
+    hostname: window.location.hostname,
+    port: window.location.port,
+    pathname: window.location.pathname,
+    search: window.location.search,
+    hash: window.location.hash,
+    origin: window.location.origin,
+  };
+};
+
+console.log(yourUrl());
