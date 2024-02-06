bc5282f23359c370d709066ce3c16efc31aa7fac
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-02-06 10:16:26 +0200

    HW38

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,11 @@
-function sum(a, b) {
-  return a + b;
-}
-sum(4, 5);
-
-function min(a, b) {
-  return a - b;
-}
-min(3, 1);
-function mult(a, b) {
-  return a * b;
-}
-mult(8, 5);
+const updateQueryString = function (url, key, value) {
+  const newUrl = new URL(url);
+  newUrl.searchParams.set(key, value);
+  return newUrl;
+};
+const result = updateQueryString(
+  "https://example.com/search?q=query&sort=ascending&page=2",
+  "mike",
+  "18",
+);
+console.log(result);
