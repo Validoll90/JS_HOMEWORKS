7322a1bac2dff9f1fb1fcae7de969f410bdee81b
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-02-07 13:38:32 +0200

    HW41

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,25 @@
-function sum(a, b) {
-  return a + b;
-}
-sum(4, 5);
+// const addOne = (x) => x + 1;
+// const square = (x) => x * x;
+// const callbacks = [addOne, square, addOne];
+// const runCallbacks = function (callbackArr, value) {
+//   return callbackArr.reduce((acc, callbacks) => callbacks(acc), value);
+// };
+// const result = runCallbacks(callbacks, 1);
+// console.log(result);
 
-function min(a, b) {
-  return a - b;
-}
-min(3, 1);
-function mult(a, b) {
-  return a * b;
-}
-mult(8, 5);
+// // // // // //
+const double = (x) => x * 2;
+const square = (x) => x * x;
+const negate = (x) => -x;
+const composeFunctions = function (...functions) {
+  return function (value) {
+    let result = value;
+    for (let i = functions.length - 1; i >= 0; i--) {
+      result = functions[i](result);
+    }
+    return result;
+  };
+};
+
+const composedFunction = composeFunctions(double, square, negate);
+console.log(composedFunction(2));
