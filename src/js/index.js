f54c47667fcf88497d9aa3b14859a9026ba3d7e2
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-02-16 10:52:44 +0200

    HW44

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,28 @@
-function sum(a, b) {
-  return a + b;
-}
-sum(4, 5);
+const flatArr = function (...arr) {
+  if (arr.length !== 1) {
+    console.error(
+      "Function accepts only 1 argument, too much arguments provided",
+    );
+  }
 
-function min(a, b) {
-  return a - b;
-}
-min(3, 1);
-function mult(a, b) {
-  return a * b;
-}
-mult(8, 5);
+  const newArr = [];
+
+  const flatter = function (arr) {
+    for (let i = 0; i < arr.length; i += 1) {
+      if (typeof arr[i] === "object") {
+        flatter(arr[i]);
+      } else newArr.push(arr[i]);
+    }
+    return newArr;
+  };
+
+  return flatter(arr);
+};
+
+const complexArray = [
+  [1, 2, [3.1, 3.2, [3.21, 3.22]], 4],
+  [5, [6, [7, [8, 9]]]],
+  [[10, 11], 12, 13],
+];
+
+console.log(flatArr(complexArray));
