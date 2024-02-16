4604569f101b47c4b4216058372bc830b2fc93ce
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-02-16 10:44:09 +0200

    HW40.

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,14 @@
 // Reduce
 
-const people = [
-  { name: "Vika", age: 20, money: 200 },
-  { name: "Vlad", age: 25, money: 300 },
-  { name: "Olga", age: 30, money: 500 },
-];
-let amount = 0;
-for (let i = 0; i < people.length; i++) {
-  amount += people[i].money;
-}
-console.log(amount);
-// or
-
-const middleAge = people.reduce(
-  (acc, currentValue) => acc + currentValue.age,
-  0,
-);
-console.log(middleAge / people.length);
+const arr = [1, 2, 3, 4, 5];
+const callback = (acc, currentValue) => acc + currentValue;
+const reduceFunc = (callback, arr = []) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    sum = callback(sum, arr[i]);
+  }
+  return sum;
+};
+console.log(reduceFunc(callback, arr));
 
 //
