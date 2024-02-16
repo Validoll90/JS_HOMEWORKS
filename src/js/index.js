3501c09a361827b918a46df673c535f78dfd6db1
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-02-16 10:39:45 +0200

    HW43

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,29 @@
-function sum(a, b) {
-  return a + b;
-}
-sum(4, 5);
-
-function min(a, b) {
-  return a - b;
-}
-min(3, 1);
-function mult(a, b) {
-  return a * b;
-}
-mult(8, 5);
+// task1
+const factorial = function (n) {
+  if (n < 0) {
+    console.error("stop");
+  } else if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+};
+console.log(factorial(3));
+//
+// task2/
+const pow = function (num, degree) {
+  if (degree === 1) {
+    return num;
+  }
+  return num * pow(num, degree - 1);
+};
+console.log(pow(2, 4));
+//
+// task3
+const sum = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return a + sum(a, b - 1);
+};
+console.log(sum(3, 6));
