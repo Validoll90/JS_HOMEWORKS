fc220311504b618de499fa540f3af2d1eadabd65
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-02-19 15:44:31 +0200

    HW49

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,16 @@
-function sum(a, b) {
-  return a + b;
-}
-sum(4, 5);
-
-function min(a, b) {
-  return a - b;
-}
-min(3, 1);
-function mult(a, b) {
-  return a * b;
-}
-mult(8, 5);
+const namesArr = ["Vika", "Vova", "Andriy", "Pavlo"];
+const result = namesArr.filter((name) => name.length >= 5);
+console.log(result);
+//
+//task 2
+const numbersArr = [1, 2, 3, 4, 5, 6];
+const result = numbersArr
+  .filter((num) => num % 2 === 0)
+  .reduce((acc, curValue) => acc + curValue, 0);
+console.log(result);
+//
+//task3
+const numbArray = [1, 2, 3, 4, 5];
+const result =
+  numbArray.reduce((acc, curVal) => acc + curVal, 0) / numbArray.length;
+console.log(result);
