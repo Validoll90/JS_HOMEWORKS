8ce4c7d152a433008a667daff35900687839fad3
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-02-19 13:02:17 +0200

    HW48

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,18 @@
-function sum(a, b) {
-  return a + b;
-}
-sum(4, 5);
-
-function min(a, b) {
-  return a - b;
-}
-min(3, 1);
-function mult(a, b) {
-  return a * b;
-}
-mult(8, 5);
+const a = (num1, num2) => num1 + num2;
+console.log(a(1, 2));
+//
+//
+const b = (str) => str.length;
+console.log(b("Vika"));
+//
+//
+const newArr = (numbers) => {
+  const result = [];
+  for (let i = 0; i < numbers.length; i++) {
+    result.push(numbers[i] + 1);
+  }
+  return result;
+};
+const numbers = [1, 2, 3];
+const bigArr = newArr(numbers);
+console.log(bigArr);
