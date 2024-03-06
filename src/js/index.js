2ef5f37618ac50ee6bf7f8fa5bcafbd326fe82a4
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-03-06 12:48:23 +0200

    HW51

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,15 @@
-function sum(a, b) {
-  return a + b;
-}
-sum(4, 5);
+const parseJSON = (jsonToString) => {
+  try {
+    return JSON.parse(jsonToString);
+  } catch {
+    return "Invalid JSON string";
+  }
+};
 
-function min(a, b) {
-  return a - b;
-}
-min(3, 1);
-function mult(a, b) {
-  return a * b;
-}
-mult(8, 5);
+console.log(parseJSON('{"name": "John", "age": 30}'));
+console.log(parseJSON('{"name": "John", "age":'));
+console.log(parseJSON('{"key": "value", "number": 123}'));
+console.log(parseJSON('{"array": [1, 2, 3]}'));
+console.log(parseJSON('{"bool": true}'));
+console.log(parseJSON("null"));
+console.log(parseJSON("undefined"));
