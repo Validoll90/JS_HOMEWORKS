d3ac201726be73bbde99c30498a5e6e6d8e64fa0
Valentyna Kolesnykova <valentinakovtan@gmail.com>
2024-03-08 12:06:50 +0200

    HW51

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,12 @@
-function sum(a, b) {
-  return a + b;
-}
-sum(4, 5);
+const arr = document.getElementById("arr");
+let count = 0;
+const itemsText = [];
 
-function min(a, b) {
-  return a - b;
+for (const li of arr.querySelectorAll("li")) {
+  console.log(li.textContent.trim());
+  count++;
+  itemsText.push((li.textContent = "Hello"));
 }
-min(3, 1);
-function mult(a, b) {
-  return a * b;
-}
-mult(8, 5);
+
+console.log(`Total elements: ${count}`);
+console.log(itemsText);
